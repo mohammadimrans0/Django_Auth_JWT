5fcca001fd12ce16c90a4433d1fd45419ed45234
Mohoammad Imran <mohoammadimran@Mohammads-MacBook.local>
2025-05-15 12:31:41 +0600

    completed post functionality with custom permission

---
+++ post/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets, permissions
+from .models import Post
+from .serializers import PostSerializer
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
